b3364fc5c5724d6faed14599bdf86c2fee34336a
Nimira43 <n.rabone72@aol.com>
2025-02-16 21:33:10 +0000

    Improved the controls slightly.

--- script.js
+++ script.js
@@ -1,18 +1,31 @@
 const char = document.getElementById('character')
 const block = document.getElementById('block')
+
 const jump = () => {
-  if (char.classList != 'animate') char.classList.add('animate')
-  char.classList.add('animate')
-  setTimeout(() => {
-    char.classList.remove('animate')
-  }, 1500)
+    if (!char.classList.contains('animate')) {
+        char.classList.add('animate');
+        setTimeout(() => {
+            char.classList.remove('animate')
+        }, 1500)
+    }
 }
-const checkCol = setInterval(() => {
-  const charTop = parseInt(window.getComputedStyle(char).getPropertyValue('top'))
-  const blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue('left'))
-  if (blockLeft < 40 &&
-    blockLeft > 0 &&
-    charTop >= 260) {
-      alert('You Dead!!!')
+
+const randomizeBlockSpeed = () => {
+    const blockAnimation = `block ${Math.random() * 2 + 1.5}s infinite`;
+    block.style.animation = blockAnimation;
+}
+
+document.addEventListener('keydown', (e) => {
+    if (e.code === 'Space') jump()
+})
+
+randomizeBlockSpeed()
+
+const checkCollision = setInterval(() => {
+    const charTop = parseInt(window.getComputedStyle(char).getPropertyValue('top'))
+    const blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue('left'))
+
+    if (blockLeft < 40 && blockLeft > 0 && charTop >= 260) {
+        alert('You Dead!!!')
     }
 }, 20)
