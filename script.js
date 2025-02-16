5a065145e6eb34ddc7f3c78335d35623a51cfc5c
Nimira43 <n.rabone72@aol.com>
2025-02-16 22:36:01 +0000

    Adding HTML for Game Over modal

--- script.js
+++ script.js
@@ -2,50 +2,50 @@ const char = document.getElementById('character')
 const game = document.getElementById('game')
 
 const jump = () => {
-    if (!char.classList.contains('animate')) {
-        char.classList.add('animate')
-        setTimeout(() => {
-            char.classList.remove('animate')
-        }, 1500)
-    }
+  if (!char.classList.contains('animate')) {
+    char.classList.add('animate')
+    setTimeout(() => {
+      char.classList.remove('animate')
+    }, 1500)
+  }
 }
 
 const createBlock = () => {
-    const block = document.createElement('div')
-    block.classList.add('block')
-    block.style.animationDuration = `${Math.random() * 1.5 + 1.5}s`
-    block.style.animationDelay = `${Math.random() * 2}s`
-    game.appendChild(block)
+  const block = document.createElement('div')
+  block.classList.add('block')
+  block.style.animationDuration = `${Math.random() * 1.5 + 1.5}s`
+  block.style.animationDelay = `${Math.random() * 2}s`
+  game.appendChild(block)
 }
 
 const manageBlocks = () => {
-    const blocks = document.querySelectorAll('.block');
-    blocks.forEach(block => {
-        const blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue('left'))
-        if (blockLeft < -40) {
-            block.remove()
-        }
-    })
-
-    if (blocks.length < 1) {
-        createBlock()
+  const blocks = document.querySelectorAll('.block');
+  blocks.forEach(block => {
+    const blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue('left'))
+    if (blockLeft < -40) {
+      block.remove()
     }
+  })
+
+  if (blocks.length < 2) {
+    createBlock()
+  }
 }
 
 document.addEventListener('keydown', (e) => {
-    if (e.code === 'Space') jump()
+  if (e.code === 'Space') jump()
 })
 
 const checkCollision = setInterval(() => {
-    const charTop = parseInt(window.getComputedStyle(char).getPropertyValue('top'))
-    const blocks = document.querySelectorAll('.block')
+  const charTop = parseInt(window.getComputedStyle(char).getPropertyValue('top'))
+  const blocks = document.querySelectorAll('.block')
 
-    blocks.forEach(block => {
-        const blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue('left'))
-        if (blockLeft < 40 && blockLeft > 0 && charTop >= 260) {
-            alert('You Dead!!!')
-        }
-    })
+  blocks.forEach(block => {
+    const blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue('left'))
+    if (blockLeft < 40 && blockLeft > 0 && charTop >= 260) {
+      alert('You Dead!!!')
+    }
+  })
 }, 20)
 
 setInterval(() => manageBlocks(), 1000)
