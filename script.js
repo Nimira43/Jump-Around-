6ac54669b9c6c3f053dc999a83ee99c084f1b395
Nimira43 <n.rabone72@aol.com>
2025-02-16 22:25:08 +0000

    Improvements to collison detection.

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const char = document.getElementById('character')
-const block = document.getElementById('block')
+const game = document.getElementById('game')
 
 const jump = () => {
     if (!char.classList.contains('animate')) {
-        char.classList.add('animate');
+        char.classList.add('animate')
         setTimeout(() => {
             char.classList.remove('animate')
         }, 1500)
@@ -11,40 +11,41 @@ const jump = () => {
 }
 
 const createBlock = () => {
-  const block = document.createElement('div')
-  block.classList.add('block')
-  block.style.animationDuration = `${Math.random() * 1.5 + 1.5}s`
-  block.style.animationDelay = `${Math.random() * 2}s`
-  game.appendChild(block)
-  return block
+    const block = document.createElement('div')
+    block.classList.add('block')
+    block.style.animationDuration = `${Math.random() * 1.5 + 1.5}s`
+    block.style.animationDelay = `${Math.random() * 2}s`
+    game.appendChild(block)
 }
 
 const manageBlocks = () => {
-  const blocks = document.querySelectorAll('.block')
-  blocks.forEach(block => {
-    const blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue('left'))
-    if (blockLeft < -40) {
-      block.remove()
+    const blocks = document.querySelectorAll('.block');
+    blocks.forEach(block => {
+        const blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue('left'))
+        if (blockLeft < -40) {
+            block.remove()
+        }
+    })
+
+    if (blocks.length < 1) {
+        createBlock()
     }
-  })
-  if (blocks.length < 4) {
-    const newBlock = createBlock()
-    setTimeout(() => manageBlocks(), 1000)
-  }
-  
 }
 
 document.addEventListener('keydown', (e) => {
     if (e.code === 'Space') jump()
 })
 
-randomiseBlockSpeed()
-
 const checkCollision = setInterval(() => {
     const charTop = parseInt(window.getComputedStyle(char).getPropertyValue('top'))
-    const blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue('left'))
+    const blocks = document.querySelectorAll('.block')
 
-    if (blockLeft < 40 && blockLeft > 0 && charTop >= 260) {
-        alert('You Dead!!!')
-    }
+    blocks.forEach(block => {
+        const blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue('left'))
+        if (blockLeft < 40 && blockLeft > 0 && charTop >= 260) {
+            alert('You Dead!!!')
+        }
+    })
 }, 20)
+
+setInterval(() => manageBlocks(), 1000)
